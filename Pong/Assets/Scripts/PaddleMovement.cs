701e6386dccd878f5ea88f57b3d48f60100c5f5f
Maxx Toskin <toskin.5@osu.edu>
2019-01-18 14:47:24 -0500

    Pong testing

---
+++ Pong/Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+    public GameObject ball;
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = (ball.transform.position - transform.position).x;
+        Vector3 targetLocation = new Vector3(ball.transform.position.x, transform.position.y, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, targetLocation, speed * Time.deltaTime);
+    }
+}
--- Pong/Assets/Scripts/PlayerTestController.cs
+++ Pong/Assets/Scripts/PlayerTestController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerTestController : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,16 @@ public class PlayerTestController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity.x = -speed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            velocity.x = speed;
+        }
+
+        transform.position += velocity * Time.deltaTime;
     }
 }
