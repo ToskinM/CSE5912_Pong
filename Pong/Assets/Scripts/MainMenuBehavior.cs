8921bd58ea95fc2dab5aab4def8ba31912578e79
Wendy Loomis <loomis.103@osu.edu>
2019-01-19 12:34:36 -0500

    added main menu
    I'll probably mess with this some more, but this is a start

---
+++ Pong/Assets/Scripts/MainMenuBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBehavior : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit(); // this doesn't affect the unity editor, only a built application
+    }
+}
