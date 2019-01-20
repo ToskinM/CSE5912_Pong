4e7f2a353e47f4233ccb0350ba53193bf3abc265
Dorothy <dordor0709@gmail.com>
2019-01-19 23:02:59 -0500

    add button listener

--- Pong/Assets/Scripts/MainMenuBehavior.cs
+++ Pong/Assets/Scripts/MainMenuBehavior.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuBehavior : MonoBehaviour
 {
+    public Button start, quit;
+    void Start()
+    {
+        start.onClick.AddListener(PlayGame);
+        quit.onClick.AddListener(QuitGame);
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -15,4 +22,5 @@ public class MainMenuBehavior : MonoBehaviour
         Debug.Log("Quit");
         Application.Quit(); // this doesn't affect the unity editor, only a built application
     }
+
 }
