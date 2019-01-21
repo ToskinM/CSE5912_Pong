11613981b5bb8e2647235531fdf95f989622f4c1
Dorothy <dordor0709@gmail.com>
2019-01-21 00:59:48 -0500

    update music for quit Nag

--- Pong/Assets/Scripts/UI/PopupBehavior.cs
+++ Pong/Assets/Scripts/UI/PopupBehavior.cs
@@ -7,23 +7,27 @@ using UnityEngine.UI;
 public class PopupBehavior : MonoBehaviour
 {
     public Button yes, no;
-    ICommand cont; 
+    ICommand cont;
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
     void Start()
     {
+        MusicSource.clip = MusicClip;
         yes.onClick.AddListener(Quit);
         no.onClick.AddListener(Continue);
-
         cont = new KillNagCommand();
     }
 
     public void Quit()
     {
+        MusicSource.Play();
         Debug.Log("Quit");
         Application.Quit(); // this doesn't affect the unity editor, only a built application
     }
 
     public void Continue()
     {
+        MusicSource.Play();
         //SceneManager.UnloadScene(SceneManager.GetActiveScene().buildIndex);
         cont.Execute();
     }
