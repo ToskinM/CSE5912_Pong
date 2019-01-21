d7b1884c28dec6d40068e8c01c1d3814e8cdd384
Dorothy <dordor0709@gmail.com>
2019-01-21 00:41:17 -0500

    uploaded music & button click sound effect
    music from https://assetstore.unity.com/packages/templates/pong-template-mobile-ready-71929

--- Pong/Assets/Scripts/UI/MainMenuBehavior.cs
+++ Pong/Assets/Scripts/UI/MainMenuBehavior.cs
@@ -7,23 +7,28 @@ using UnityEngine.UI;
 public class MainMenuBehavior : MonoBehaviour
 {
     public Button start, quit;
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
     public ICommand play, nag; 
     void Start()
     {
         start.onClick.AddListener(PlayGame);
         quit.onClick.AddListener(QuitGame);
-
+        MusicSource.clip = MusicClip;
         play = new PlayCommand();
         nag = new NagCommand(); 
     }
     public void PlayGame()
     {
-        play.Execute(); 
+        MusicSource.Play();
+        play.Execute();
     }
 
     public void QuitGame()
     {
+        MusicSource.Play();
         nag.Execute();
+
         //Application.Quit(); // this doesn't affect the unity editor, only a built application
     }
 
