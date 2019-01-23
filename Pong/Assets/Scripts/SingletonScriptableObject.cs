2483d0b4b6fb11ba86665355265bd045f98d7a61
Maxx Toskin <toskin.5@osu.edu>
2019-01-23 16:05:37 -0500

    Persistent scene optimization, added logo

---
+++ Pong/Assets/Scripts/SingletonScriptableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class SingletonScriptableObject<T> : ScriptableObject where T : ScriptableObject
+{
+    static T instance = null;
+    public static T Instance
+    {
+        get
+        {
+            if (!instance)
+                instance = Resources.FindObjectsOfTypeAll<T>().FirstOrDefault();
+            return instance;
+        }
+    }
+}
