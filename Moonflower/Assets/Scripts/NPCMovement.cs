cc3d346d0b066eb751164cff0fda6aa4f10abf7e
Wendy Loomis <loomis.103@osu.edu>
2019-01-26 23:57:57 -0500

    more nav

---
+++ Moonflower/Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; 
+
+public class NPCMovement : MonoBehaviour
+{
+    public bool Wandering { get; set; }
+
+    public NavMeshAgent agent;
+    public GameObject plane;
+
+    Vector3 dest;
+    const float bufferDist = 0.2f;
+    int count = 0;
+    int threshhold;
+    const int smallPause = 30, largePause = 80;
+
+    private void Start()
+    {
+        Wandering = true;
+        dest = getRandomDest();
+        transform.position = dest;
+        threshhold = getRandomPause(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(dest);
+        Debug.Log(transform.position); 
+
+        if (Wandering)
+        {
+            if (Vector3.Distance(agent.transform.position, dest) <= bufferDist)
+            {
+                Debug.Log("New Dest");
+                dest = getRandomDest();
+                agent.SetDestination(dest);
+            }
+        }
+    }
+
+    Vector3 getRandomDest()
+    {
+        float x = Random.Range(plane.transform.position.x - plane.transform.localScale.x,
+            plane.transform.position.x + plane.transform.localScale.x);
+        float z = Random.Range(plane.transform.position.z - plane.transform.localScale.z,
+            plane.transform.position.z + plane.transform.localScale.z);
+        return new Vector3(x, 0, z); 
+    }
+
+    int getRandomPause()
+    {
+        return Random.Range(smallPause, largePause); 
+    }
+    
+}
