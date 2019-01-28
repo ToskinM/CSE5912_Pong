9f327ab0203a46a06f934fe1b5221cdc9ed5aab1
Dorothy <dordor0709@gmail.com>
2019-01-28 09:22:03 -0500

    add mimbi following anai

---
+++ Moonflower/Assets/Scripts/MimbiMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MimbiMovement : MonoBehaviour
+{
+    public GameObject targetObject;
+    private Vector3 velocity = Vector3.zero;
+    public float smoothTime = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //targetObject = GameObject.FindGameObjectWithTag("Pyayer");
+        //if (targetObject==null)
+        //{
+        //    Debug.Log("can't find anai");
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(targetObject.transform.position.x+1, targetObject.transform.position.y-0.5f, targetObject.transform.position.z-1), ref velocity, smoothTime);
+    }
+}
