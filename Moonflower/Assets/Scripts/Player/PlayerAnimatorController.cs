b79a02e5e925aa7c4079974ec3f3f0861cee739a
Dana Brooks <danacarolynbrooks@gmail.com>
2019-02-01 22:52:14 -0500

    adds running capability/animation

---
+++ Moonflower/Assets/Scripts/Player/PlayerAnimatorController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimatorController : MonoBehaviour
+{
+    private const string key_isRun = "IsRun";
+    private const string key_isWalk = "IsWalk";
+    private const string key_isAttack01 = "IsAttack01";
+    private const string key_isAttack02 = "IsAttack02";
+    private const string key_isJump = "IsJump";
+    private const string key_isDamage = "IsDamage";
+    private const string key_isDead = "IsDead";
+    private Animator animator;
+    private PlayerCombatController combatController;
+    private PlayerMovement movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        movement = GetComponent<PlayerMovement>();
+        combatController = GetComponent<PlayerCombatController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (combatController.inCombat)
+        {
+            animator.SetBool(key_isAttack02, true);
+        }
+        else
+        {
+            animator.SetBool(key_isAttack02, false);
+        }
+        if (movement.walking)
+        {
+            animator.SetBool(key_isWalk, true);
+        }
+        else
+        {
+            animator.SetBool(key_isWalk, false);
+        }
+        if (movement.running)
+        {
+            print("SETRUN");
+            animator.SetBool(key_isRun, true);
+        }
+        else
+        {
+            animator.SetBool(key_isRun, false);
+        }
+    }
+}
--- Moonflower/Assets/Scripts/Player/PlayerMovement.cs
+++ Moonflower/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,17 +5,23 @@ using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed;
+    private float moveSpeed;
+    public float runSpeed;
+    public float walkSpeed;
     public float rotateSpeed = 20f;
     public bool playing;
     public bool inputDetected = false;
     public GameObject camera;
+    public bool walking;
+    public bool running;
 
     // Start is called before the first frame update
     void Start()
     {
         playing = true;
-        moveSpeed = 5f;
+        moveSpeed = 3f;
+        walkSpeed = 3f;
+        runSpeed = 7f;
     }
     void DetectKeyInput()
     {
@@ -27,17 +33,33 @@ public class PlayerMovement : MonoBehaviour
         {
             //Quaternion rotation = Quaternion.AngleAxis(camera.transform.rotation.eulerAngles.y, Vector3.up);
             //transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                running = true;
+                walking = false;
+                moveSpeed = runSpeed;
+            }
+            else
+            {
+                if (!running ||Input.GetKeyUp(KeyCode.LeftShift))
+                {
+                    walking = true;
+                    running = false;
+                    moveSpeed = walkSpeed;
+                }
 
+            }
             float angle = Mathf.Atan2(horizontalInput, verticalInput) * (180 / Mathf.PI);
             angle += camera.transform.rotation.eulerAngles.y;
             //Debug.Log(angle);
             Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
             transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
-
             inputDetected = true;
         }
         else
         {
+            running = false;
+            walking = false;
             inputDetected = false;
         }
 
@@ -53,14 +75,18 @@ public class PlayerMovement : MonoBehaviour
         //}
 
         Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput);
+
         transform.Translate(new Vector3(0f, 0f, Vector3.Magnitude(direction)) * Time.deltaTime * moveSpeed);
     }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Switch"))
         { playing = !playing; }
-        if(playing)
+        if (playing)
+        {
             DetectKeyInput();
+        }
     }
 }
