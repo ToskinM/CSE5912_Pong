2290faa9d43e80a088b8123256e6f90b37366c64
Dorothy <dordor0709@gmail.com>
2019-02-01 19:39:23 -0500

    giveup using raycast

--- Moonflower/Assets/Scripts/Player/PickupArea.cs
+++ Moonflower/Assets/Scripts/Player/PickupArea.cs
@@ -32,8 +32,43 @@ public class PickupArea : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.gameObject.name);
+        Debug.Log("in trigger Enter");
+        Debug.Log(other.gameObject.name + " is collected ");
+        Destroy(other.gameObject,1);
     }
+    //void RaycastCollision()
+    //{
+    //    RaycastHit hit;
+    //    //collision detection with obstacle
+
+    //    for (int i = 0; i < 90; i+=10)
+    //    {
+    //        for (int j = -90; j < 90; j+=10)
+    //        {
+    //            //Vector3 worldDirection = Quaternion.Euler(localEulerAngles) * transform.forward;
+    //            //Quaternion angle = Quaternion.Euler(i, j, 0);
+
+    //            Quaternion spreadAngleX = Quaternion.AngleAxis(i, new Vector3(1, 0, 0));
+    //            Quaternion spreadAngleY = Quaternion.AngleAxis(j, new Vector3(0, 1, 0));
+    //            Quaternion QuaResult = spreadAngleX * spreadAngleY;
+
+
+    //            Quaternion angle = Quaternion.Euler(i, j, 0);
+    //            Vector3 newVector = angle * transform.forward;
+    //            //draw the ray 
+    //            Debug.DrawRay(transform.position, newVector * 2, Color.black);
+
+
+    //            if (Physics.Raycast(transform.position, transform.TransformDirection(newVector), out hit, 2f))
+    //            {
+    //                Debug.Log(hit.collider.gameObject.name);
+    //            }
+    //        }
+    //    }
+
+    //}
+
+
     // Update is called once per frame
     void Update()
     {
