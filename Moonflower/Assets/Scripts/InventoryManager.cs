efe1c0642fe171d797c32d802bed66029d76b55d
Dorothy <dordor0709@gmail.com>
2019-02-02 14:58:44 -0500

    updated simple inventory

---
+++ Moonflower/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public Inventory[] items;
+    public static InventoryManager instance;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public float getHealth(string name)
+    {
+        Inventory i = Array.Find(items, Inventory => Inventory.name == name);
+        if (i == null)
+        {
+            Debug.Log("there is nth");
+        }
+        return i.Health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
