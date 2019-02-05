b61f313a2d6ce443e3f9e1354dcc927b49b61cf3
Maxx Toskin <toskin.5@osu.edu>
2019-02-05 16:42:50 -0500

    bug fix and started blocking

---
+++ Moonflower/Assets/Scripts/NPC/NPCAnimationController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCAnimationController : MonoBehaviour
+{
+    private const string key_isHit = "IsHit";
+
+    private Animator animator;
+    private NPCCombatController combatController;
+    private NPCMovement movement;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        movement = GetComponent<NPCMovement>();
+        combatController = GetComponent<NPCCombatController>();
+    }
+
+    void Update()
+    {
+    }
+
+    public void SetHit(int hit)
+    {
+        if (hit == 1)
+        {
+            animator.SetBool(key_isHit, true);
+        }
+        else
+        {
+            animator.SetBool(key_isHit, false);
+        }
+    }
+}
--- Moonflower/Assets/Scripts/NPC/NPCCombatController.cs
+++ Moonflower/Assets/Scripts/NPC/NPCCombatController.cs
@@ -9,6 +9,7 @@ public class NPCCombatController : MonoBehaviour, ICombatant
     public bool IsBlocking { get; private set; }
     public bool hasWeaponOut;
     public bool inCombat; // (aggrod)
+    public bool isHit;
     //public bool isBlocking;
     public bool isAttacking;
     //public GameObject hitIndicator;
@@ -19,12 +20,14 @@ public class NPCCombatController : MonoBehaviour, ICombatant
     private float hurtDelay = 0.5f;
 
     private FieldOfView fieldOfView;
+    private NPCAnimationController npcAnimationController;
     private GameObject combatTarget;
 
     void Start()
     {
         Stats = gameObject.GetComponent<CharacterStats>();
         fieldOfView = GetComponent<FieldOfView>();
+        npcAnimationController = GetComponent<NPCAnimationController>();
     }
 
     void Update()
@@ -79,6 +82,8 @@ public class NPCCombatController : MonoBehaviour, ICombatant
                 }
                 else
                 {
+                    npcAnimationController.SetHit(1);
+
                     Debug.Log(gameObject.name + " took " + damage + " damage from " + source.name);
                     Stats.TakeDamage(damage);
                     CheckDeath();
