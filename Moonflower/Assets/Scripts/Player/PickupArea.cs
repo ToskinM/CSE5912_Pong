66e2d61a82a519d9c679adef4184ddb6b36cd844
Dorothy <dordor0709@gmail.com>
2019-02-10 00:13:08 -0500

    can only pickup one item per time

--- Moonflower/Assets/Scripts/Player/PickupArea.cs
+++ Moonflower/Assets/Scripts/Player/PickupArea.cs
@@ -11,6 +11,8 @@ public class PickupArea : MonoBehaviour
     private PlayerInventory playerInventory;
     private AudioManager audioManager;
 
+    private List<GameObject> currentCollisions = new List<GameObject>();
+    private GameObject[] allCollidable;
     void Start()
     {
         FindObjectOfType<AudioManager>().Play("background");
@@ -31,33 +33,50 @@ public class PickupArea : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-
         if (other.gameObject.tag == "Collectable")
         {
+            //Get closest items
+            GameObject obj = FindClosest();
             //Get items's health
-            int health = other.gameObject.GetComponent<InventoryStat>().GetHealth();
-            //Destroy Gameobject after collect
-            Destroy(other.gameObject);
+            int health = obj.GetComponent<InventoryStat>().GetHealth();
 
-            if (other.gameObject.GetComponent<InventoryStat>().AnaiObject)
+            if (obj.GetComponent<InventoryStat>().AnaiObject)
             {
-                Debug.Log(other.gameObject.tag + " is collected, " + health + " were add to anai");
-                playerInventory.AddMoonFlower(other.gameObject);
+                Debug.Log(obj.tag + " is collected, " + health + " were add to anai");
+                playerInventory.AddMoonFlower(obj.gameObject);
                 playerStat.AddHealth(health);
             }
-            else
+            else if (obj.GetComponent<InventoryStat>().MimbiObject)
             {
-                Debug.Log(other.gameObject.tag + " is collected, " + health + " were add to mimmbi");
-                playerInventory.AddWolfApple(other.gameObject);
+                Debug.Log(obj.tag + " is collected, " + health + " were add to mimmbi");
+                playerInventory.AddWolfApple(obj.gameObject);
             }
+            //Destroy Gameobject after collect
+            Destroy(obj.gameObject);
+            //Play Pickup audio clip
             audioManager.Play("pickup01");
         }
+
     }
 
+    private GameObject FindClosest()
+    {
+        allCollidable = GameObject.FindGameObjectsWithTag("Collectable");
+        GameObject nearestObj = allCollidable[0];
+        float nearest = Vector3.Distance(allCollidable[0].transform.position,transform.position);
+        foreach (GameObject g in allCollidable)
+        {
+            if (Vector3.Distance(g.transform.position, transform.position) < nearest)
+            {
+                nearest = Vector3.Distance(g.transform.position, transform.position);
+                nearestObj = g;
+            }   
+        }
+        return nearestObj;
+    }
     // Update is called once per frame
     void Update()
     {
-      
     }
 }
 
