2c2f42c60eaae7eec7278d56dff6a00cabfc696f
Evan McWilliams <mcwilliams.68@osu.edu>
2019-02-10 16:12:01 -0500

    Added Key and Gate

---
+++ Moonflower/Assets/Scripts/GateforKey.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateforKey : MonoBehaviour
+{
+    private bool opened = false;
+    public GameObject target;
+    private int openTime = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(openTime != 0)
+        {
+            openTime--;
+            if(openTime == 0)
+            {
+                TimedClose();
+            }
+        }
+    }
+
+    public void OpenUp()
+    {
+        if (!opened)
+        {
+            transform.position += new Vector3(0, 4, 0);
+            opened = true;
+        }
+    }
+
+
+    public void TimedOpenUp(int time)
+    {
+        openTime = time;
+        if(!opened)
+        {
+            transform.position += new Vector3(0, 4, 0);
+            opened = true;
+        }
+    }
+
+    public void TimedClose()
+    {
+        transform.position += new Vector3(0, -4, 0);
+        opened = false;
+    }
+}
---
+++ Moonflower/Assets/Scripts/KeyForGate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyForGate : MonoBehaviour
+{
+    public GameObject target;
+    public GateforKey gate;
+    public int time = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gate = target.GetComponent<GateforKey>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.Equals(GameObject.Find("Anai").GetComponent<BoxCollider>()) || other.Equals(GameObject.Find("Mimbi").GetComponent<BoxCollider>()))
+        {
+            if (time != 0)
+            {
+                gate.TimedOpenUp(time);
+            }
+            else
+            {
+                gate.OpenUp();
+            }
+        }
+    }
+}
