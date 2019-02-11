eec8eb82c5afa9ffcbd658647e6640b36b7e4faf
Dana Brooks <danacarolynbrooks@gmail.com>
2019-02-11 18:36:43 -0500

    Scaled time day/night cycle

--- Moonflower/Assets/Scripts/LightColor.cs
+++ Moonflower/Assets/Scripts/LightColor.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LightColor : MonoBehaviour
 {
-
+    int[] times;
     Color[] colors;
     Vector3[] positions;
     Vector3[] rotations;
@@ -16,54 +16,62 @@ public class LightColor : MonoBehaviour
     void Start()
 
     {
-
         colors = new Color[7];
         positions = new Vector3[7];
         rotations = new Vector3[7];
         intensities = new float[7];
+        times = new int[7]; //how many updates to spend on each color
         light = GetComponent<Light>();
 
-        //orange - sunrise (6am)
+        //orange - sunrise (5-6am)
         colors[0] = new Color(1, .5f, 0);
         positions[0] = new Vector3(-24, 24, 104);
         rotations[0] = new Vector3(8, 188, 352);
         intensities[0] = 1f;
+        times[0] = 1; //spend 1 hour on sunrise
 
-        //light purple - morning
+        //light purple - morning(6-10)
         colors[1] = new Color(.8f, .7f, .9f);
         positions[1] = new Vector3(-30, 49, 104);
         rotations[1] = new Vector3(31, 183, 359);
         intensities[1] = 1f;
+        times[1] = 4; //spend 4 hours on morning
 
-        //light yellow - afternoon
+        //light yellow - afternoon(11-4)
         colors[2] = new Color(1, 1, .8f);
         positions[2] = new Vector3(-30, 134, -3);
         rotations[2] = new Vector3(74, 188, 4);
         intensities[2] = 1f;
+        times[2] = 5; //spend 5 hours on early-mid afternoon
+        
 
-        //yellow - late afternoon/evening
+        //yellow - late afternoon/evening (4-7)
         colors[3] = new Color(.9f, 1, .5f);
         positions[3] = new Vector3(-30, 134, -140);
         rotations[3] = new Vector3(50, 354, 174);
         intensities[3] = 1f;
+        times[3] = 3; //spend 3 hours on late afternoon
 
-        //magenta - sunset
+        //magenta - sunset (7-8pm)
         colors[4] = new Color(1, 0, .2f);
         positions[4] = new Vector3(-30, 29, -202);
         rotations[4] = new Vector3(16, 358, 176);
         intensities[4] = 1f;
+        times[4] = 1; //spend 1 hour on sunset
 
-        //Dark purple - early night
-        colors[5] = new Color(.5f, 0, .7f);
+        //Dark purple - early night (8-11)
+        colors[5] = new Color(.3f, .15f, .5f);
         positions[5] = new Vector3(-30, 134, -3);
         rotations[5] = new Vector3(74, 188, 4);
         intensities[5] = .6f;
+        times[5] = 3; //spend 3 hours on early night
 
-        //Dark blue - late night
+        //Dark blue - late night (11pm-5am)
         colors[6] = new Color(.3f, .3f, .5f);
         positions[6] = new Vector3(-30, 49, 104);
         rotations[6] = new Vector3(31, 183, 359);
         intensities[6] = 1f;
+        times[6] = 6; //spend 6 hours on night;
 
 
         transform.position = positions[0];
@@ -106,11 +114,11 @@ public class LightColor : MonoBehaviour
                 light.intensity = Mathf.Lerp(light.intensity, intensities[counter], Time.deltaTime * transitionRate);
                 light.color = Color.Lerp(light.color, colors[counter], Time.deltaTime * transitionRate);
 
-                transitionRate += Time.deltaTime / transitionTime;
+                transitionRate += Time.deltaTime / (transitionTime * times[counter]);
 
                 yield return null;
             }
-            if (counter + 1 > 6)
+            if (counter + 1 == colors.Length)
             {
                 counter = 0;
             }
--- Moonflower/Assets/Scripts/SkyColors.cs
+++ Moonflower/Assets/Scripts/SkyColors.cs
@@ -7,14 +7,16 @@ public class SkyColors : MonoBehaviour
 
     Color[] colors;
     float[] fogHeights;
+    int[] times;
     MeshRenderer thisRend;
     int counter = 0;
     float transitionTime = 5f; // Amount of time it takes to fade between colors
 
+
     void Start()
 
     {
-
+        times = new int[7]; //how many updates to spend on each color
         thisRend = GetComponent<MeshRenderer>();
 
         colors = new Color[7];
@@ -24,23 +26,36 @@ public class SkyColors : MonoBehaviour
         //orange - sunrise (6am)
         colors[0] = new Color(.92f, .16f, .2f);
         fogHeights[0] = 1f;
+        times[0] = 1; //spend 1 hour on sunrise
+
         //light purple - morning
         colors[1] = new Color(.9f, .6f, 1);
         fogHeights[1] = .7f;
+        times[1] = 4; //spend 4 hours on morning
+
         //grey - afternoon
         colors[2] = new Color(.75f, .75f, .75f);
         fogHeights[2] = .1f;
+        times[2] = 5; //spend 5 hours on early-mid afternoon
+
         //dark grey - late afternoon/evening
         colors[3] = new Color(.5f, .5f, .5f);
         fogHeights[3] = .4f;
+        times[3] = 3; //spend 3 hours on late afternoon
+
         //magenta - sunset
         colors[4] = new Color(.92f, .16f, .2f);
         fogHeights[4] = .9f;
+        times[4] = 1; //spend 1 hour on sunset
+
         //Dark purple - early night
         colors[5] = new Color(.7f, .08f, .4f);
         fogHeights[5] = .1f;
+        times[5] = 3; //spend 3 hours on early night
+
         colors[6] = new Color(.2f, .11f, .27f);
         fogHeights[6] = .1f;
+        times[6] = 6; //spend 6 hours on night;
 
         thisRend.material.SetColor("_TintColor", colors[0]);
         StartCoroutine(ColorChange());
@@ -74,11 +89,10 @@ public class SkyColors : MonoBehaviour
                 thisRend.material.SetColor("_TintColor", Color.Lerp(thisRend.material.GetColor("_TintColor"), newColor, Time.deltaTime * transitionRate));
                 thisRend.material.SetFloat("_FogHeight", Mathf.Lerp(thisRend.material.GetFloat("_FogHeight"), newFog, Time.deltaTime * transitionRate));
 
-                transitionRate += Time.deltaTime / transitionTime;
-
+                transitionRate += Time.deltaTime / (transitionTime * times[counter]);
                 yield return null;
             }
-            if (counter + 1 > 6)
+            if (counter + 1 > colors.Length)
             {
                 counter = 0;
             }
