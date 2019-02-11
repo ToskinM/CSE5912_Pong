4514df0ecd436eee6cec795e323ae61f329680ab
Dana Brooks <danacarolynbrooks@gmail.com>
2019-02-11 01:11:48 -0500

    Atmosphere fog for basic sky colors

--- Moonflower/Assets/Scripts/SkyColors.cs
+++ Moonflower/Assets/Scripts/SkyColors.cs
@@ -7,7 +7,7 @@ public class SkyColors : MonoBehaviour
 
     Color[] colors;
     float[] fogHeights;
-    MeshRenderer thisRend; //Renderer of our Cube
+    MeshRenderer thisRend;
     int counter = 0;
     float transitionTime = 5f; // Amount of time it takes to fade between colors
 
@@ -15,20 +15,13 @@ public class SkyColors : MonoBehaviour
 
     {
 
-        thisRend = GetComponent<MeshRenderer>(); // grab the renderer component on our cube
+        thisRend = GetComponent<MeshRenderer>();
 
-        colors = new Color[7]; // We will randomize through this array
+        colors = new Color[7];
         fogHeights = new float[7];
-        /* colors[0] = Color.blue;
-         colors[1] = Color.red;
-         colors[2] = Color.yellow;
-         colors[3] = Color.magenta;
-         colors[4] = Color.green;
-         colors[5] = Color.cyan;
-         colors[6] = Color.red;*/
 
 
-        //orange - sunrise
+        //orange - sunrise (6am)
         colors[0] = new Color(.92f, .16f, .2f);
         fogHeights[0] = 1f;
         //light purple - morning
@@ -45,13 +38,11 @@ public class SkyColors : MonoBehaviour
         fogHeights[4] = .9f;
         //Dark purple - early night
         colors[5] = new Color(.7f, .08f, .4f);
-        fogHeights[5] = .5f;
+        fogHeights[5] = .1f;
         colors[6] = new Color(.2f, .11f, .27f);
         fogHeights[6] = .1f;
-        //
 
-        //start our coroutine when the game starts
-
+        thisRend.material.SetColor("_TintColor", colors[0]);
         StartCoroutine(ColorChange());
 
     }
@@ -75,20 +66,13 @@ public class SkyColors : MonoBehaviour
             Color newColor = colors[counter];
             float newFog = fogHeights[counter];
 
-            float transitionRate = 0; //Create and set transitionRate to 0. This is necessary for our next while loop to function
-
-            /* 1 is the highest value that the Color.Lerp function uses for
-
-             * transitioning between two colors. This while loop will execute
-
-             * until transitionRate is incremented to 1 or higher
-
-             */
+            float transitionRate = 0; 
+            
 
             while (transitionRate < 1)
             {
                 thisRend.material.SetColor("_TintColor", Color.Lerp(thisRend.material.GetColor("_TintColor"), newColor, Time.deltaTime * transitionRate));
-                //thisRend.sharedMaterial.SetFloat("_FogHeight", Mathf.Lerp(thisRend.sharedMaterial.GetFloat("_FogHeight"), newFog, Time.deltaTime * transitionRate));
+                thisRend.material.SetFloat("_FogHeight", Mathf.Lerp(thisRend.material.GetFloat("_FogHeight"), newFog, Time.deltaTime * transitionRate));
 
                 transitionRate += Time.deltaTime / transitionTime;
 
