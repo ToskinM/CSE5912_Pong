349c9d30510225ccb025cffd64018a2d892ad20e
Maxx Toskin <toskin.5@osu.edu>
2019-02-25 20:16:04 -0500

    fixed dialoguecam errors

--- Moonflower/Assets/Scripts/Camera/DialogueCamera.cs
+++ Moonflower/Assets/Scripts/Camera/DialogueCamera.cs
@@ -4,45 +4,45 @@ using UnityEngine;
 
 public class DialogueCamera : MonoBehaviour
 {
-    public Transform target;
-    public float switchTime = 0.3f;
+    //public Transform target;
+    //public float switchTime = 0.3f;
 
-    void Start()
-    {
-        
-    }
+    //void Start()
+    //{
 
-    private void OnEnable()
-    {
-        StartCoroutine(MoveCameraToNewTarget(GetCameraTarget(GameObject.FindGameObjectWithTag("Player"))));
-    }
+    //}
 
-    void Update()
-    {
-        
-    }
+    //private void OnEnable()
+    //{
+    //    StartCoroutine(MoveCameraToNewTarget(GetCameraTarget(GameObject.FindGameObjectWithTag("Player"))));
+    //}
 
-    private IEnumerator MoveCameraToNewTarget(Transform newTarget)
-    {
-        switchTransform.position = target.position;
-        Vector3 startingPosition = target.position;
-        target = switchTransform;
+    //void Update()
+    //{
 
-        float currentAngleY = transform.eulerAngles.y;
-        float startingXRotation = xRotation;
+    //}
 
-        // Interpolate target position and xRotation to new target
-        float t = 0;
-        while (t < switchTime)
-        {
-            xRotation = Mathf.LerpAngle(startingXRotation, startingXRotation + (newTarget.eulerAngles.y - currentAngleY), t / switchTime);
-            switchTransform.position = Vector3.Lerp(startingPosition, newTarget.position, t / switchTime);
+    //private IEnumerator MoveCameraToNewTarget(Transform newTarget)
+    //{
+    //    switchTransform.position = target.position;
+    //    Vector3 startingPosition = target.position;
+    //    target = switchTransform;
 
-            t += Time.deltaTime * 1;
-            yield return null;
-        }
+    //    float currentAngleY = transform.eulerAngles.y;
+    //    float startingXRotation = xRotation;
 
-        target = newTarget;
-        switchTransform.position = transform.position;
-    }
+    //    Interpolate target position and xRotation to new target
+    //    float t = 0;
+    //    while (t < switchTime)
+    //    {
+    //        xRotation = Mathf.LerpAngle(startingXRotation, startingXRotation + (newTarget.eulerAngles.y - currentAngleY), t / switchTime);
+    //        switchTransform.position = Vector3.Lerp(startingPosition, newTarget.position, t / switchTime);
+
+    //        t += Time.deltaTime * 1;
+    //        yield return null;
+    //    }
+
+    //    target = newTarget;
+    //    switchTransform.position = transform.position;
+    //}
 }
