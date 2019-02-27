b2935929ca1020761e0fbff244803b95f966b947
Maxx Toskin <toskin.5@osu.edu>
2019-02-27 16:14:35 -0500

    Defence integration

--- Moonflower/Assets/Scripts/NPC/NPCGroup.cs
+++ Moonflower/Assets/Scripts/NPC/NPCGroup.cs
@@ -6,6 +6,7 @@ public class NPCGroup : MonoBehaviour
 {
     public string groupName;
     public List<LesserNPCController> NPCs;
+
     public bool assistInCombat;
     public bool destroyIfEmpty;
 
