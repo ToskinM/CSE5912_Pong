151481ca5da0ddd8107b08aaa898cc56a4a3a43e
Dorothy <dordor0709@gmail.com>
2019-03-19 15:38:07 -0400

    item can pickup

--- Moonflower/Assets/Scripts/Player/PlayerMovementController.cs
+++ Moonflower/Assets/Scripts/Player/PlayerMovementController.cs
@@ -224,7 +224,7 @@ public class PlayerMovementController : MonoBehaviour
         {
             Jumping = true;
             onGround = false;
-            if (Action != Actions.Running)
+            if (Action != Actions.Running && LevelManager.current.currentPlayer.name == "Anai")
             {
                 body.AddForce(new Vector3(0f, 30f, 0f), ForceMode.Impulse);
             }
