3089c2ce945f6d789b81337241af136e1f30e4a9
Maxx Toskin <toskin.5@osu.edu>
2019-03-25 15:26:38 -0400

    Bats get random colored material

---
+++ Moonflower/Assets/Scripts/NPC/MaterialRandomizer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialRandomizer : MonoBehaviour
+{
+    public Material[] materials;
+    public Renderer renderer;
+
+    void Start()
+    {
+        renderer.material = materials[(int)Random.Range(0f, materials.Length - 1)];
+    }
+}
