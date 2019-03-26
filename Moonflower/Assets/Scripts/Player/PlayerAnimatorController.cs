8b7f47e7562068b336af25a23ef5c5243b014c00
Dorothy <dordor0709@gmail.com>
2019-03-26 16:21:39 -0400

    Mimbi can change back to normal pose after switching to anai

--- Moonflower/Assets/Scripts/Player/PlayerAnimatorController.cs
+++ Moonflower/Assets/Scripts/Player/PlayerAnimatorController.cs
@@ -79,20 +79,21 @@ public class PlayerAnimatorController : MonoBehaviour
 
     public void EnableDistraction()
     {
+        animator.SetBool(key_isDistract, true);
+        animator.SetBool(key_isWalk, false);
+    }
+
+    public void DisableDistraction()
+    {
         if (playerController.AnaiIsActive())
         {
             companionAnimator.SetBool(key_isDistract, false);
+            companionAnimator.SetBool(key_isWalk, false);
         }
         else
         {
             animator.SetBool(key_isDistract, false);
         }
-    }
-
-    public void DisableDistraction()
-    {
-
-        animator.SetBool(key_isDistract, false);
     }
 
     public void TriggerDeath()
