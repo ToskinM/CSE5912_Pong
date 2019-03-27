6992ffeafe9812a0866828581bcd587a2a02eec9
Joel Wong <35244327+joelwjx@users.noreply.github.com>
2019-03-26 22:06:05 -0400

    More scene transition fixes

---
+++ Moonflower/Assets/Scripts/Player/AnaiSingleton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnaiSingleton : MonoBehaviour
+{
+    void Awake()
+    {
+        // If I exist already, destroy me.  There can only be one.
+        if (FindObjectOfType<AnaiSingleton>() == this)
+        {
+            DontDestroyOnLoad(gameObject.transform);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Moonflower/Assets/Scripts/Player/MimbiSingleton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MimbiSingleton : MonoBehaviour
+{
+    void Awake()
+    {
+        // If I exist already, destroy me.  There can only be one.
+        if (FindObjectOfType<MimbiSingleton>() == this)
+        {
+            DontDestroyOnLoad(gameObject.transform);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
