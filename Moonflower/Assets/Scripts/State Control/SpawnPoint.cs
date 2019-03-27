173dcc8be1dd54fedb8a967de720a07705f16360
Joel Wong <35244327+joelwjx@users.noreply.github.com>
2019-03-26 23:36:21 -0400

    Only let active player into interior scenes and disables companion

--- Moonflower/Assets/Scripts/State Control/SpawnPoint.cs
+++ Moonflower/Assets/Scripts/State Control/SpawnPoint.cs
@@ -15,7 +15,7 @@ public class SpawnPoint : MonoBehaviour
         if (thisScene.Equals("The Village"))
         {
             PlayerController.instance.AnaiObject.transform.position = this.transform.position;
-            PlayerController.instance.MimbiObject.transform.position = this.transform.position + new Vector3(0, 2, 0);
+            PlayerController.instance.MimbiObject.transform.position = this.transform.position + new Vector3(0, 0, 2);
         }
     }
     // Update is called once per frame
