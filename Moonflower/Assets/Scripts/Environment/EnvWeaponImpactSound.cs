82a97fcc7ef1eb07a1700a66bbc4126fc695468b
Joel Wong <35244327+joelwjx@users.noreply.github.com>
2019-04-03 23:32:14 -0400

    Added environmental sound effects to crystals

---
+++ Moonflower/Assets/Scripts/Environment/EnvWeaponImpactSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvWeaponImpactSound : MonoBehaviour
+{
+    public AudioClip[] ImpactSoundsList;
+
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PlayerHurtbox")
+        {
+            audioSource.clip = ImpactSoundsList[Random.Range(0, ImpactSoundsList.Length - 1)];
+            audioSource.Play();
+        }
+    }
+}
