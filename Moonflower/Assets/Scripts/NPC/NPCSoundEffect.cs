6296388eca910ea4a1983ec8e8e8358ba08fc1ed
Dorothy <dordor0709@gmail.com>
2019-04-06 22:16:19 -0400

    audio error fix in village

--- Moonflower/Assets/Scripts/NPC/NPCSoundEffect.cs
+++ Moonflower/Assets/Scripts/NPC/NPCSoundEffect.cs
@@ -9,7 +9,7 @@ public class NPCSoundEffect : MonoBehaviour
     private readonly string NPC_Bat = "NPC Bat";
     private readonly string NPC_Human = "NPC Human";
     private readonly string NPC_Orby = "NPC Orby";
-
+    private readonly string NPC_Teju = "NPC BigMouse";
 
     // Start is called before the first frame update
     void Start()
@@ -100,7 +100,9 @@ public class NPCSoundEffect : MonoBehaviour
 
     public string DecideWhichNPC()
     {
-        if (gameObject.name.Contains("Mouse"))
+        if (gameObject.name.Contains("Teju"))
+            return NPC_Teju;
+        else if (gameObject.name.Contains("Mouse"))
             return NPC;
         else if (gameObject.name.Contains("Bat"))
             return NPC_Bat;
