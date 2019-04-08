aac8f968c1b7545f058c02d339858449b250a642
Maxx Toskin <toskin.5@osu.edu>
2019-04-08 15:11:47 -0400

    Super Awesome Cheese Lovin' Lads

---
+++ Moonflower/Assets/SpawnerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerController : MonoBehaviour
+{
+    public NPCSpawner[] spawners;
+
+    void Start()
+    {
+        
+    }
+
+    public void ActivateAll()
+    {
+        for (int i = 0; i < spawners.Length; i++)
+        {
+            spawners[i].Activate();
+        }
+    }
+    public void DeactivateAll()
+    {
+        for (int i = 0; i < spawners.Length; i++)
+        {
+            spawners[i].Deactivate();
+        }
+    }
+}
