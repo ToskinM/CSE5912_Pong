c355fa50db67114a2eb9561647d6981c53818f45
Joel Wong <35244327+joelwjx@users.noreply.github.com>
2019-04-07 22:15:30 -0400

    Player low health alert overlay

---
+++ Moonflower/Assets/Scripts/UI/HealthAlertOverlay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthAlertOverlay : MonoBehaviour
+{
+    CharacterStats playerStats;
+    Image overlay;
+    bool displaying;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerStats = PlayerController.instance.ActivePlayerStats;
+        overlay = GetComponent<Image>();
+        displaying = false;
+
+        Color overlayC = overlay.color;
+        overlayC.a = 1.0f;
+        overlay.color = overlayC;
+
+        overlay.canvasRenderer.SetAlpha(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hpFrac = 1.0f * playerStats.CurrentHealth / playerStats.MaxHealth;
+
+        if (hpFrac <= 0.2f && !displaying)
+        {
+            overlay.CrossFadeAlpha(1f, 1f, true);
+            displaying = true;
+        }   
+        else if (hpFrac > 0.2f && displaying)
+        {
+            overlay.CrossFadeAlpha(0f, 1f, true);
+            displaying = false;
+        }
+    }
+}
