37f16150ac688d51eb43967bdc079e00cad541ac
Dorothy <dordor0709@gmail.com>
2019-04-08 18:09:31 -0400

    message when went to far

---
+++ Moonflower/Assets/Fence.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Fence : MonoBehaviour
+{
+    public FeedbackText Feedback;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Feedback = GameObject.Find("FeedbackText").GetComponent<FeedbackText>();
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == PlayerController.instance.GetActivePlayerObject())
+        Feedback.ShowText("It is dangerous to go that far");
+    }
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
