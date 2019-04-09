8a95f9f6109336ba3081ce751f3bc3e3a94680c8
Maxx Toskin <toskin.5@osu.edu>
2019-04-08 20:33:21 -0400

    Credits complete?

---
+++ Moonflower/Assets/CreditsCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsCamera : MonoBehaviour
+{
+    public float panSpeed = 1;
+    [Range(0, 1)] public float recenterTimeMultiplier = 1;
+    public float yRotationClamp = 5; // degrees
+    public float xRotationClamp = 5; // degrees
+
+    private float inputRotationX;
+    private float inputRotationY;
+
+    void Update()
+    {
+        // Get input look adjustment
+        inputRotationX = Mathf.Clamp(inputRotationX + Input.GetAxis("Mouse Y"), -yRotationClamp, yRotationClamp);
+        inputRotationY = Mathf.Clamp(inputRotationY + Input.GetAxis("Mouse X"), -xRotationClamp, xRotationClamp);
+
+        // Get the position and rotation at that percentage
+        Quaternion newRotation = Quaternion.Euler(-inputRotationX, inputRotationY, 0f);
+
+        // Smoothly match those values
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * panSpeed);
+
+        // Smoothly keep camera centered
+        inputRotationX *= 0.96f * recenterTimeMultiplier;
+        inputRotationY *= 0.96f * recenterTimeMultiplier;
+    }
+}
--- Moonflower/Assets/CreditsSequence.cs
+++ Moonflower/Assets/CreditsSequence.cs
@@ -4,64 +4,19 @@ using UnityEngine;
 
 public class CreditsSequence : MonoBehaviour
 {
-    public float startDelay = 1f;
-    public float scrollSpeed = 0.5f;
+    public float creditsDuration;
 
-    public CanvasGroup[] displayGroups;
-    public Transform startPoint;
-    public Transform midPoint;
-    public Transform endPoint;
-
-    private CanvasGroup currentlyDisplayedGroup;
-
-    void Start()
-    {
-        // Hide all groups to begin with
-        for (int i = 0; i < displayGroups.Length; i++)
-        {
-            displayGroups[i].alpha = 0;
-        }
-
-        StartCoroutine(BeginSequence());
-    }
+    private float time;
 
     void Update()
     {
+        time += Time.deltaTime;
 
-    }
-
-    private IEnumerator BeginSequence()
-    {
-        // Initial delay
-        yield return new WaitForSeconds(startDelay);
-
-        // 
-        for (int i = 0; i < displayGroups.Length; i++)
+        if (time >= creditsDuration)
         {
-            yield return StartCoroutine(GroupPass(displayGroups[i]));
-        }
-    }
+            time = 0;
 
-    private IEnumerator GroupPass(CanvasGroup group)
-    {
-        Transform groupTransform = group.transform;
-
-        while (Vector3.Distance(group.transform.position, endPoint.position) > 0.1f)
-        {
-            groupTransform.position = Vector3.MoveTowards(groupTransform.position, endPoint.position, scrollSpeed);
-            group.alpha = Mathf.Clamp((1 - Vector3.Distance(groupTransform.position, midPoint.position) / Vector3.Distance(startPoint.position, midPoint.position)), 0f, 1f);
-
-            yield return null;
-        }
-    }
-
-    private IEnumerator Fade(CanvasGroup canvasGroup, float finalAlpha, float fadeDuraton = 1)
-    {
-        float fadeSpeed = Mathf.Abs(canvasGroup.alpha - finalAlpha) / fadeDuraton;
-        while (!Mathf.Approximately(canvasGroup.alpha, finalAlpha))
-        {
-            canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, finalAlpha, fadeSpeed * Time.deltaTime);
-            yield return null;
+            SceneController.current.FadeAndLoadSceneNoLS(Constants.SCENE_MAINMENU);
         }
     }
 }
