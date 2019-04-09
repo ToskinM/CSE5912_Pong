7a42e125aac1b1df4ff6c54eb68fa71d5016c53b
Joel Wong <35244327+joelwjx@users.noreply.github.com>
2019-04-09 00:00:07 -0400

    Cheat code to get that bread

--- Moonflower/Assets/Scripts/Boss/Teju/TejuController.cs
+++ Moonflower/Assets/Scripts/Boss/Teju/TejuController.cs
@@ -57,7 +57,7 @@ public class TejuController : MonoBehaviour, INPCController
         Vector3 walkOrigin = transform.position;
 
         LevelManager.current.RegisterNPC(gameObject);
-        //feedback = GameObject.Find("FeedbackText").GetComponent<FeedbackText>();
+        feedback = GameObject.Find("FeedbackText").GetComponent<FeedbackText>();
 
         actionsAvailable = new bool[] { canInspect, canTalk, canDistract, canGift };
 
