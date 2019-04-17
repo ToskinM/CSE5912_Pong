83589bbf4bcdca3e6c03f24e89a62c763f50c785
Dorothy <dordor0709@gmail.com>
2019-04-17 14:19:20 -0400

    more persistence item

---
+++ Moonflower/Assets/Scripts/Inventory/AnaiItemReload.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnaiItemReload : MonoBehaviour
+{
+    private GameObject anaiHouseItem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anaiHouseItem = GameObject.Find("AnaiHouseItems");
+        SetActiveTrue(anaiHouseItem);
+    }
+    public void SetActiveTrue(GameObject SceneItem)
+    {
+        if (SceneItem != null)
+        {
+            foreach (Transform child in SceneItem.transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+
+    }
+}
