805db00cb70219bb866d6cb9429df07ad7ce1c81
Dana Brooks <danacarolynbrooks@gmail.com>
2019-04-18 22:31:57 -0400

    mimbi particles in village

---
+++ Moonflower/Assets/Scripts/Player/MimbiParticles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MimbiParticles : MonoBehaviour
+{
+    public GameObject walkParticles;
+    public GameObject runParticles;
+    public GameObject standingParticles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
