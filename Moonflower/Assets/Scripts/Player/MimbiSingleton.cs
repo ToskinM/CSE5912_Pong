73e141ed0a3247d295ed99d71a0f18d74b1c784b
Joel Wong <35244327+joelwjx@users.noreply.github.com>
2019-04-22 22:46:10 -0400

    Platforming lava now deals damage

--- Moonflower/Assets/Scripts/Player/MimbiSingleton.cs
+++ Moonflower/Assets/Scripts/Player/MimbiSingleton.cs
@@ -14,9 +14,12 @@ public class MimbiSingleton : MonoBehaviour
             instance = this;
 
             // MIMBI IS A HIGH MAINTENANCE DOGE
-            if (UnityEngine.SceneManagement.SceneManager.GetSceneAt(1).name == Constants.SCENE_ANAIHOUSE)
+            if (UnityEngine.SceneManagement.SceneManager.sceneCount > 1)
             {
-                gameObject.SetActive(false);
+                if (UnityEngine.SceneManagement.SceneManager.GetSceneAt(1).name == Constants.SCENE_ANAIHOUSE)
+                {
+                    gameObject.SetActive(false);
+                }
             }
         }
         else if (instance != this)
